3cbe604f14e32fb2703353eb644eea263c477818
Pieter <pietergeurts23@gmail.com>
2026-01-12 11:27:34 -0500

    made http proxy faster

--- SSLStrip.py
+++ SSLStrip.py
@@ -6,29 +6,36 @@ import ssl
 LISTEN_HOST = "0.0.0.0"
 LISTEN_PORT = 8080
 
-def fetch_https(host, path=b"/"):
-	# testing webservice uses a self signed certificate which python WILL endlessly complain about
-	context = ssl._create_unverified_context()
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	tls = context.wrap_socket(sock, server_hostname=host)
-	tls.connect((host, 443))
+host_to_tls = {}
+hosts_using_https = set()
+
+def should_use_https(host):
+	return host in hosts_using_https
+
+def fetch_https(host, path):
+	global host_to_tls
+
+	if host in host_to_tls:
+		tls = host_to_tls[host]
+	else:
+		context = ssl._create_unverified_context()
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		tls = context.wrap_socket(sock, server_hostname=host)
+		tls.connect((host, 443))
+		host_to_tls[host] = tls
 
 	req = b"GET " + path + b" HTTP/1.1\r\n"
 	req += b"Host: " + host.encode() + b"\r\n"
-	req += b"Connection: close\r\n\r\n"
+	req += b"Connection: keep-alive\r\n\r\n"
 
-	tls.sendall(req)
+	try:
+		tls.sendall(req)
+	except:
+		del host_to_tls[host]
+		return fetch_https(host, path)
 
-	response = b""
-	while True:
-		data = tls.recv(8192)
-		if not data:
-			break
-		response += data
-
-	tls.close()
+	response = read_http_response(tls)
 	return response
-
 
 def ssl_strip(data):
 	data = data.replace(b"https://", b"http://")
@@ -40,7 +47,6 @@ def ssl_strip(data):
 	)
 
 	return data
-
 
 def is_https_redirect(data):
 	return (
@@ -55,8 +61,7 @@ def http_proxy(client_sock):
 		client_sock.close()
 		return
 
-	print("REQUEST:\n", request)
-
+	print(request)
 	headers = request.split(b"\r\n")
 	host = None
 	path = b"/"
@@ -75,29 +80,60 @@ def http_proxy(client_sock):
 		client_sock.close()
 		return
 
+	if host in hosts_using_https:
+		response = fetch_https(host, path)
+		client_sock.sendall(ssl_strip(response))
+		client_sock.close()
+		return
+
 	server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_sock.connect((host, 80))
 	server_sock.sendall(request)
 
-	response = b""
-	while True:
-		data = server_sock.recv(8192)
-		if not data:
-			break
-		response += data
-
+	response = read_http_response(server_sock)
+	print(response)
 	server_sock.close()
 
-	print("RESPONSE:\n", response)
-
-	# slow but historically accurate
 	if is_https_redirect(response):
-		https_response = fetch_https(host, path)
-		client_sock.sendall(ssl_strip(https_response))
-	else:
-		client_sock.sendall(ssl_strip(response))
+		hosts_using_https.add(host)
+		response = fetch_https(host, path)
 
+	client_sock.sendall(ssl_strip(response))
 	client_sock.close()
+	
+def read_http_response(sock):
+	data = b""
+
+	# read all headers (\r\n\r\n terminates headers)
+	while b"\r\n\r\n" not in data:
+		chunk = sock.recv(4096)
+		if not chunk:
+			break
+		data += chunk
+		
+	if b"\r\n\r\n" not in data:
+		# incomplete response
+		return data
+
+	headers, body = data.split(b"\r\n\r\n", 1)
+
+	content_length = None
+	for line in headers.split(b"\r\n"):
+		if line.lower().startswith(b"content-length:"):
+			content_length = int(line.split(b":", 1)[1].strip())
+			break
+	# TODO maybe do something if content length is None or negative
+
+	# read the rest of the body
+	remaining = content_length - len(body)
+	while remaining > 0:
+		chunk = sock.recv(min(4096, remaining))
+		if not chunk:
+			break
+		body += chunk
+		remaining -= len(chunk)
+
+	return headers + b"\r\n\r\n" + body
 
 
 def start_proxy():
